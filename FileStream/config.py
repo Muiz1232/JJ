bb5e782867a85f5134a4709303ed5881d6a61bf7
Muiz1232 <167227414+Muiz1232@users.noreply.github.com>
2024-09-20 13:18:58 -0700

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -16,12 +16,12 @@ class Telegram:
     FORCE_SUB = env.get('FORCE_UPDATES_CHANNEL', True)
     FORCE_SUB = True if str(FORCE_SUB).lower() == "true" else False
     SLEEP_THRESHOLD = int(env.get("SLEEP_THRESHOLD", "60"))
-    FILE_PIC = env.get('FILE_PIC', "https://graph.org/file/5bb9935be0229adf98b73.jpg")
-    START_PIC = env.get('START_PIC', "https://graph.org/file/290af25276fa34fa8f0aa.jpg")
-    VERIFY_PIC = env.get('VERIFY_PIC', "https://graph.org/file/736e21cc0efa4d8c2a0e4.jpg")
+    FILE_PIC = env.get('FILE_PIC', "https://amshop.cloud/test/uploads/teleservices_66edd863896de4.18136180.jpg")
+    START_PIC = env.get('START_PIC', "https://amshop.cloud/test/uploads/teleservices_66edd863896de4.18136180.jpg")
+    VERIFY_PIC = env.get('VERIFY_PIC', "https://amshop.cloud/test/uploads/teleservices_66edd863896de4.18136180.jpg")
     MULTI_CLIENT = False
-    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", None))   # Logs channel for file logs
-    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", None))   # Logs channel for user logs
+    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", '-1002358747296'))   # Logs channel for file logs
+    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", '-1002358747296'))   # Logs channel for user logs
     MODE = env.get("MODE", "primary")
     SECONDARY = True if MODE.lower() == "secondary" else False
     AUTH_USERS = list(set(int(x) for x in str(env.get("AUTH_USERS", "")).split()))
