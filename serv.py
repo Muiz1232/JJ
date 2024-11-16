f3c3e6b21e88f7b5994bc75e627895b0d6ccf382
Muiz1232 <167227414+Muiz1232@users.noreply.github.com>
2024-11-17 03:03:55 +0500

    Update serv.py

--- serv.py
+++ serv.py
@@ -6,7 +6,7 @@ starttime = time.time()
 
 while True:
     try:
-        response = requests.get('https://jj-wzj6.onrender.com/dl/34')  # Use localhost or a valid address
+        response = requests.get('https://jj-wzj6.onrender.com/status')  # Use localhost or a valid address
         print(f"Status Code: {response.status_code}, Time: {time.ctime()}")
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
